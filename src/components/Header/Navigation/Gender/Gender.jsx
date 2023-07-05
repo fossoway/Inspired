9c085a8bc882a4c45845ce183f03438e2531ec79
Elena Bukina <selenaastu@gmail.com>
2023-07-05 18:56:15 +0400

    update gender and navigation

--- src/components/Header/Navigation/Gender/Gender.jsx
+++ src/components/Header/Navigation/Gender/Gender.jsx
@@ -1,5 +1,20 @@
+import s from './Gender.module.scss'
+
+const list = [
+    {link: 'woman', title: 'Женщины'},
+    {link: 'man', title: 'Мужчины'},
+]
+
 export const Gender = () => {
     return (
-        <div>Gender</div>
+        <ul className={s.gender}>
+            {list.map(item => (
+                <li key={item.link} className={s.item}>
+                    <a className={s.link} href={item.link}>
+                        {item.title}
+                    </a>
+                </li>
+            ))}
+        </ul>
     )
 }
--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,13 +1,14 @@
 import { Gender } from './Gender/Gender.jsx'
 import { Category } from './Category/Category.jsx'
+import { Container } from '../../Layout/Container/Container.jsx'
 
 export const Navigation = () => {
     return (
         <nav>
-            <div className="container">
+            <Container>
                 <Gender />
                 <Category />
-            </div>
+            </Container>
         </nav>
     )
 }
